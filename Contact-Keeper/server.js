9ba9b01025f6d432775ae9a55b5c9fc4caa2dc9c
kaustubh <kaustubh229@gmail.com>
2020-11-11 23:53:25 +0530

    initial commit

--- Contact-Keeper/server.js
+++ Contact-Keeper/server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const connectDB = require('./config/db')
+const path = require('path')
 
 const server = express()
 
@@ -7,11 +8,16 @@ connectDB()
 
 server.use(express.json({ extended: false }))
 
-server.get('/', (req, res) => res.json({ "hello": "Server is now live"}))
-
 server.use('/api/users', require('./routes/user'))
 server.use('/api/auth', require('./routes/auth'))
 server.use('/api/contacts', require('./routes/contact'))
+
+// Serve Static Assets
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static('client/build'))
+
+    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')))
+}
 
 const PORT = process.env.PORT || 5000
 
